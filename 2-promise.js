c57be3ce695489ac48efc5f7266e0f1f7e8d2eb2
mnaofficial <mna.official92@gmail.com>
2022-09-02 11:08:09 +0600

    promise ,done

--- 2-promise.js
+++ 2-promise.js
@@ -17,7 +17,7 @@ const userIds = [1, 2, 3, 4, 5];
 let userData = [];
 for (let i = 0; i < userIds.length; i++) {
     const userId = userIds[i];
-    userData.push(myPromise)
+    userData.push(myPromise);
 }
 
 Promise.all(userData).then(res=>{
