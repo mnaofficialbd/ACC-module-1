56c0a776f1d5126bcd4e93f95f605c77fc42763c
mnaofficial <mna.official92@gmail.com>
2022-09-02 11:27:13 +0600

    async await function

--- 3-AsyncAwait.js
+++ 3-AsyncAwait.js
@@ -1,12 +1,22 @@
 const promise = new Promise((resolve, reject) => {
-    setTimeout(()=>{
+    setTimeout(() => {
         resolve('Success')
-    },1000)
+    }, 1000)
 })
 
-async function getData(){
-   const res= await promise;
-   console.log(res);
+async function getData() {
+    const res = await promise;
+    console.log(res);
 }
 
 getData();
+
+async function getUser() {
+    const res = await fetch('');
+    const user = await res.json();
+    console.log(user);
+
+    const res2 = await fetch(`https://www.example.com/${user.id}`);
+    const product = await res2.json()
+    console.log(product);
+}
