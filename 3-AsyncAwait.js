67214d547c4440f8f73ad74de18c2911b619ad4c
mnaofficial <mna.official92@gmail.com>
2022-09-02 11:14:48 +0600

    async await function create

---
+++ 3-AsyncAwait.js
@@ -0,0 +1,10 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        resolve('Success')
+    },1000)
+})
+
+async function getData(){
+   const res= await promise;
+   console.log(res);
+}
